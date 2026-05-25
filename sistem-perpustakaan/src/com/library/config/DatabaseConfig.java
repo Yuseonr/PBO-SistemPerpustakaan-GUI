175fe77bb4232b6db5e2b9ec0bcf63aedd63a5c9
Yuseonr <rafianandra@gmail.com>
2026-05-26 01:21:45 +0700

    del : arcid

--- sistem-perpustakaan/src/com/library/config/DatabaseConfig.java
+++ sistem-perpustakaan/src/com/library/config/DatabaseConfig.java
@@ -15,9 +15,9 @@ import java.sql.SQLException;
  */
 public class DatabaseConfig {
     // Konfigurasi koneksi database
-    private static final String URL = "jdbc:mysql://localhost:3306/lib_test?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+    private static final String URL = "jdbc:mysql://localhost:3307/lib_test?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
     private static final String USER = "root";
-    private static final String PASSWORD = "12345";
+    private static final String PASSWORD = "Tambahkan";
 
     // agar tidak di inisialisasi
     private DatabaseConfig() {
